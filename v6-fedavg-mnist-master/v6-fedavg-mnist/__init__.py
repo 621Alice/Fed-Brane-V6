f694a4f58113119313767f42c141072fe5e9a218
621Alice <15251608@life.hkbu.edu.hk>
2021-08-19 07:09:10 +0200

    Add files via upload

---
+++ v6-fedavg-mnist-master/v6-fedavg-mnist/__init__.py
@@ -0,0 +1,236 @@
+import time
+
+from vantage6.tools.util import info
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import pandas as pd
+
+import torch.optim as optim
+from vantage6.tools.util import info, warn
+from torchvision import transforms
+from torchvision import datasets, transforms
+from sklearn.model_selection import train_test_split
+from collections import OrderedDict
+import numpy as np
+
+def master(client, data, ids,epoch_per_round):
+    """Combine partials to global model
+    """
+
+    info('Collecting participating organizations')
+
+    # Collect all organization that participate in this collaboration.
+    # These organizations will receive the task to compute the partial.
+    # organizations = client.get_organizations_in_my_collaboration()
+    # ids = [organization.get("id") for organization in organizations]
+
+
+
+    # Determine the device to train on
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # Initialize model and send parameters of server to all workers
+    model = Net().to(device)
+
+    # Train without federated averaging
+    info('Train_test')
+    task = client.create_new_task(
+        input_={
+            'method': 'train_test',
+            'kwargs': {
+                'model': model,
+                'parameters': list(model.parameters()),
+                'device': device,
+                'log_interval': 10,
+                'return_params': True,
+                'epoch': epoch_per_round,
+                'if_test': False
+            }
+        }, organization_ids=ids
+    )
+
+    info("Waiting for results")
+    task_id = task.get("id")
+    task = client.get_task(task_id)
+    while not task.get("complete"):
+        task = client.get_task(task_id)
+        info("Waiting for results")
+        time.sleep(1)
+
+
+    info("Obtaining results")
+
+    results = client.get_results(task_id=task.get("id"))
+
+    global_sum = 0
+    global_count = 0
+
+
+    for output in results:
+        # print(len(output))
+        global_sum += output["params"]
+        global_count += len(global_sum)
+
+
+    averaged_parameters = global_sum / global_count
+
+    # in order to not have the optimizer see the new parameters as a non-leaf tensor, .clone().detach() needs
+    # to be applied in order to turn turn "grad_fn=<DivBackward0>" into "grad_fn=True"
+    averaged_parameters = [averaged_parameters.clone().detach()]
+
+    info('Federated averaging w/ averaged_parameters')
+    task = client.create_new_task(
+        input_={
+            'method': 'train_test',
+            'kwargs': {
+                'model': output['model'],
+                'parameters': averaged_parameters,
+                'device': device,
+                'log_interval': 10,
+                'return_params': True,
+                'epoch': epoch_per_round,
+                'if_test': True
+            }
+        },
+        organization_ids=ids
+    )
+
+    info("Waiting for results")
+    task_id = task.get("id")
+    task = client.get_task(task_id)
+    while not task.get("complete"):
+        task = client.get_task(task_id)
+        info("Waiting for results")
+        time.sleep(1)
+
+    info("Obtaining results")
+
+    results = client.get_results(task_id=task.get("id"))
+    for output in results:
+        acc = output["test_accuracy"]
+    return acc
+
+
+
+def RPC_train_test(data, model, parameters, device, log_interval, return_params, epoch, if_test):
+    """Compute the average partial
+    """
+    train = data
+    print(len(train))
+    train_batch_size = 64
+    test_batch_size = 64
+
+    X = (train.iloc[:,1: ].values).astype('float32')
+    Y = train.iloc[:,0].values
+    print(X.shape)
+    features_train, features_test, targets_train, targets_test = train_test_split(X, Y, test_size=0.3,
+                                                                                  shuffle=False)
+    X_train = torch.from_numpy(features_train/255.0)
+    X_test = torch.from_numpy(features_test/255.0)
+
+    Y_train = torch.from_numpy(targets_train).type(torch.LongTensor)
+    Y_test = torch.from_numpy(targets_test).type(torch.LongTensor)
+
+    train = torch.utils.data.TensorDataset(X_train, Y_train)
+    test = torch.utils.data.TensorDataset(X_test, Y_test)
+
+    train_loader = torch.utils.data.DataLoader(train, batch_size=train_batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=test_batch_size, shuffle=False)
+
+    #if input is train.pt
+    # train_loader = data
+
+    learning_rate = 0.01
+
+    # initializing optimizer and scheduler
+    optimizer = optim.SGD(parameters, lr=learning_rate, momentum=0.5)
+
+    model.train()
+    for epoch in range(1, epoch + 1):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+
+            batch_size = data.shape[0]
+            # print(batch_size)
+            data = data.reshape(batch_size, 28, 28)
+            data = data.unsqueeze(1)
+            # print(data.shape)
+            # print(data.type())
+            # print(target.type())
+            output = model(data)
+
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                           100. * batch_idx / len(train_loader), loss.item()))
+
+
+    test_accuracy=0
+    if if_test:
+        model.eval()
+
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                # Send the local and target to the device (cpu/gpu) the model is at
+                data, target = data.to(device), target.to(device)
+                # Run the model on the local
+                batch_size = data.shape[0]
+                # print(batch_size)
+                data = data.reshape(batch_size, 28, 28)
+                data = data.unsqueeze(1)
+                output = model(data)
+                # Calculate the loss
+                test_loss += F.nll_loss(output, target, reduction='sum').item()
+                # Check whether prediction was correct
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+            test_loss /= len(test_loader.dataset)
+
+            print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+                    test_loss, correct, len(test_loader.dataset),
+                    100. * correct / len(test_loader.dataset)))
+            test_accuracy = 100. * correct / len(test_loader.dataset)
+
+
+    if return_params:
+        for parameters in list(model.parameters()):
+            return {'params': parameters,
+                    'model': model,
+                    'test_accuracy': test_accuracy}
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
---
+++ v6-fedavg-mnist-master/v6-fedavg-mnist/example.py
@@ -0,0 +1,27 @@
+from vantage6.tools.mock_client import ClientMockProtocol
+
+
+
+client = ClientMockProtocol(
+    datasets=["./local/mnist_train_test_header_1.csv","./local/mnist_train_test_header_2.csv"],
+    # datasets=["./local/training.pt"],
+    module="v6-fedavg-mnist"
+)
+
+# organizations = client.get_organizations_in_my_collaboration()
+# org_ids = ids = [organization["id"] for organization in organizations]
+# print(org_ids)
+# print(type(org_ids))
+master_task = client.create_new_task(
+    input_={
+                "master": 1,
+                "method":"master",
+                'kwargs': {
+                    'ids':[0,1],
+                    'epoch_per_round': 1
+                }
+
+
+    }, organization_ids=[0,1])
+results = client.get_results(master_task.get("id"))
+print(results)
